4c2e180282e6cd36d2827cede1421bdf8c01e9b9
DanyaRinz <145994849+DanyaRinz@users.noreply.github.com>
2023-09-25 16:08:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     char first[255], last[255];
 
-<<<22212121
+<e57458567456
     gets(last); /* buffer overflow? what's that? */
 
     printf("Hello %s %s!\n", first, last);
